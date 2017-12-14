0a613ca77828c75e091185ac26dcee390d9b623b
TheRealIzzi <israel.peck@gmail.com>
2017-12-14 16:40:25 -0600

    More updates, still not working... MUCH GNASHING OF TEETH

--- models/burgers.js
+++ models/burgers.js
@@ -7,13 +7,11 @@ var burger = {
 			cb(res);
 		});
 	},
-
 	create: function(cols, vals, cb) {
 		orm.create("burgers", cols, vals, function(res) {
 			cb(res);
 		})
 	},
-
 	update: function(objColVals, condition, cb) {
 		orm.update("burgers", objColVals, condition, function(res) {
 			cb(res);
